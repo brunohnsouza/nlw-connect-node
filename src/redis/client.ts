3ea15f44556d6b0d503432a8501152e14b728e23
Bruno <brunohenriquehns@gmail.com>
2025-02-19 12:26:41 -0300

    Add event erro in ioredis

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -4,3 +4,15 @@ import { env } from '../env'
 export const redis = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: null,
 })
+
+redis.on('error', err => {
+  throw err
+})
+async function initializeRedis() {
+  await redis.connect()
+  await redis.set('foo', 'bar')
+}
+
+initializeRedis().catch(err => {
+  console.error('Failed to initialize Redis:', err)
+})
